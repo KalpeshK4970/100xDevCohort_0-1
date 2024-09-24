09e7c95ab3c07043cc97995681d9f624e0bcb80b
Kalpesh Kumawat <kumawat.kalpesh4970@gmail.com>
2024-09-24 23:16:31 +0530

    map & filter revision

--- Week2/revision.js
+++ Week2/revision.js
@@ -32,3 +32,16 @@ promisifiedTimeout(2000).then(function(){
         console.log("second is done");
     })
 })
+
+const input = [1,2,3,4,5];
+
+const newArr = input.map((ele,i) => {
+    return ele*2 + i;
+})
+
+const newArr2 = input.filter((ele) => {
+    return ele%2 === 0;
+})
+
+console.log(newArr2);
+console.log(newArr);
